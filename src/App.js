ba194664c29c4d4898448c54bed107422262a75e
jialan06 <126742524+jialan06@users.noreply.github.com>
2023-04-15 17:24:32 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="card-info">
+  <div className="top" />
+  <div className="content">
+    <div className="photo">
+      <div className="photoupload" />
+    </div>
+    <div className="title">
+      <div className="postmark">
+        <div className="stamp" />
+        <div className="logo" />
+      </div>
+      <div className="name">Judy</div> 
+      <div className="departmentname">
+      Department of Accounting
+      </div>
+    </div>
+    <div className="text">
+      <div classname="wishtext">
+      Congratulations on your graduation! As you embark on the next
+      chapter of your life, my wish for you is that you continue to work
+      hard, pursue your passions, and never lose sight of your goals. May
+      you have the courage to take on new challenges and the resilience to
+      overcome any obstacles that come your way. Remember to always stay
+      curious, keep learning, and never stop growing. I wish you all the
+      success and happiness in the world as you take on this exciting new
+      journey.</div>
+
+    </div>
+  </div>
+  <div className="bottom">
+    <div className="pencil-icon">
+      <div className="home-1" />
+      <div class="Vector-1"></div>
+    </div>
+    <div className="home-icon">
+      <div className="home-2" />
+      <div class="Vector-2"></div>
+    </div>
+    <div className="location-icon">
+      <div className="home-3" />
+      <div class="Vector-3"></div>
+    </div>
+  </div>
+</div>
+
+
+  );
+}
