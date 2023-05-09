f2aad933e978164bf80cdaab58a6e39a96c95f57
jialan06 <126742524+jialan06@users.noreply.github.com>
2023-05-10 01:59:08 +0800

    update

--- week8App/vite.config.js
+++ week8App/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/myAPP/',
+  base: '/week6-practice/',
   build: {
     outDir: 'docs'
   } 
